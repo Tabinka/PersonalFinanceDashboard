bde3071bcb9e007b5bdd73600dab184014ce893e
Gabriela <gabriela.szczepankova@comverga.com>
2024-07-03 19:46:00 +0200

    Creating basic layout structure

--- basic_layout.py
+++ basic_layout.py
@@ -1,5 +1,5 @@
-from dash import dcc, html, dash_table
-from starting_layout import StartingLayout
+import pandas as pd
+from dash import dcc, html, dash_table, Output
 
 class BasicLayout:
       def __init__(self, data, app):
@@ -20,3 +20,18 @@ class BasicLayout:
 
                   html.Hr(),  # horizontal line
             ])
+            
+      def register_callbacks(self):
+            @self.app.callback(
+                  Output('income-expense-chart', 'figure'),
+                  Output('expense-category-pie-chart', 'figure'),
+                  Output('monthly-savings-chart', 'figure'),
+                  Output('expense-trends-chart', 'figure'),
+                  Output('top-expenses-table', 'data')
+            )
+            def update_output():
+                  if self.data is not None:
+                        df = self.data
+                        # Data preprocessing
+                        df['Date'] = pd.to_datetime(df['Date'])
+                        df['Month'] = df['Date'].dt.to_period('M')
