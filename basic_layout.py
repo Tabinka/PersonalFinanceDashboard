c761f6ed6961b7156e7a640ce100f7f149e36338
Gabriela <gabriela.szczepankova@comverga.com>
2024-07-08 17:37:19 +0200

    Add another graphs and tables for my app

--- basic_layout.py
+++ basic_layout.py
@@ -14,6 +14,8 @@ class BasicLayout:
             savings_chart = self.savings(df_income_expense)
             self.data.drop(columns=['Transaction detail 1', 'Transaction detail 2', 'Transaction detail 3', 'Date'], inplace=True)
             category_expense = self.category_expense(self.data)
+            expense_trends = self.expense_trends(self.data)
+            top_expense_items = self.top_expense_items(self.data)
             return html.Div([
                   html.H5("Render saved file"),
                   dash_table.DataTable(
@@ -23,7 +25,9 @@ class BasicLayout:
                   html.Hr(),
                   dcc.Graph(id='income-expense-chart', figure=income_expense_chart),
                   dcc.Graph(id='monthly-savings-chart', figure=savings_chart),
-                  dcc.Graph(id='category-expense-chart', figure=category_expense)])
+                  dcc.Graph(id='category-expense-chart', figure=category_expense),
+                  dcc.Graph(id='expense-trends-chart', figure=expense_trends),
+                  dash_table.DataTable(id='top-expenses-table', figure=top_expense_items)])
             
       def income_expense(self, df):
             df['Date'] = pd.to_datetime(df['Date'])
@@ -58,3 +62,19 @@ class BasicLayout:
             values='Amount',
             title='Category-wise Expenses'
                   )
+            
+      def expense_trends(self, df):
+            expense_trends = df[df['Amount'] < 0].groupby(['Month', 'Category']).agg({'Amount': 'sum'}).reset_index()
+            expense_trends['Amount'] = expense_trends['Amount'].abs()
+            return px.line(
+            expense_trends,
+            x='Month',
+            y='Amount',
+            color='Category',
+            title='Expense Trends'
+                  )
+            
+      def top_expense_items(self, df):
+            top_expenses = df[df['Amount'] < 0].nsmallest(10, 'Amount')
+            top_expenses_table = top_expenses.to_dict('records')
+            return top_expenses_table
